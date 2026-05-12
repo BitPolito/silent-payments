f1ed193944cc4b6124da6d682dcf97d2ace65b17
Pietro <p.valese@studenti.unipi.it>
2026-05-12 10:57:02 +0200

    release bin

--- api/vanity/target/release/deps/find_msvc_tools-fde3f568e01ad728.d
+++
@@ -1,9 +0,0 @@
-/home/vcastald/bitpolito/silent-payments/api/vanity/target/release/deps/find_msvc_tools-fde3f568e01ad728.d: /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/home/vcastald/bitpolito/silent-payments/api/vanity/target/release/deps/libfind_msvc_tools-fde3f568e01ad728.rlib: /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/home/vcastald/bitpolito/silent-payments/api/vanity/target/release/deps/libfind_msvc_tools-fde3f568e01ad728.rmeta: /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs:
-/home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs:
-/home/vcastald/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs:
